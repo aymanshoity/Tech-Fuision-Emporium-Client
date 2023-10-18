d3916087d40332920390469bd1e3da9136951774
Syeda Umme Ayman <aymanshoity@gmail.com>
2023-10-18 23:54:33 +0600

    Banner designing is completed

--- src/Components/HomePage/Home.jsx
+++ src/Components/HomePage/Home.jsx
@@ -7,7 +7,6 @@ import Footer from "./Footer";
 const Home = () => {
     return (
         <div>
-            <h2>welcome to the brand shop</h2>
             <Banner></Banner>
             <AllBrands></AllBrands>
             <ChooseUs></ChooseUs>
